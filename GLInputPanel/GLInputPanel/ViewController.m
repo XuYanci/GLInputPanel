d9683f3dc6b60d34ba8a1a226b1dd83890e52ff4
XuYanci <grandy.wind@gmail.com>
2017-08-24 20:16:21 +0800

    add license

--- GLInputPanel/GLInputPanel/ViewController.m
+++ GLInputPanel/GLInputPanel/ViewController.m
@@ -20,7 +20,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
     /** 添加悬浮视图 */
     [self.view addSubview:self.floatView];
     [self.floatView resetPosition];
